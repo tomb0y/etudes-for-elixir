176f5371595be7691c39c72f3abba6949a3fc46a
zs <baloghzsof@gmail.com>
2017-04-08 12:06:28 +0200

    Implement Etudes.Geom area/2

--- lib/etudes/geom.ex
+++ lib/etudes/geom.ex
@@ -1,4 +1,9 @@
 defmodule Etudes.Geom do
-  def area(_length, _width) do
+  @moduledoc false
+
+  @spec area(number(), number()) :: number()
+
+  def area(rectangle_length \\ 1, rectangle_width \\ 1) do
+    rectangle_length * rectangle_width
   end
 end
--- test/geom_test.exs
+++ test/geom_test.exs
@@ -1,13 +1,30 @@
 defmodule GeomTest do
   use ExUnit.Case
 
-  test 'there is a geom module' do
+  test "there is a geom module" do
     Etudes.Geom.module_info()
   end
 
-  test 'the geom module has the area/2 function' do
+  test "the geom module has the area/2 function" do
     geom_methods = Etudes.Geom.module_info(:exports)
 
     assert Enum.member?(geom_methods, { :area, 2 })
+  end
+
+  test "geom area/2 | yields the area of a rectangle" do
+    assert Etudes.Geom.area(3, 4) == 12
+    assert Etudes.Geom.area(12, 7) == 84
+    assert Etudes.Geom.area(1.5, 2) == 3
+    assert Etudes.Geom.area(4, 1.5) == 6
+    assert Etudes.Geom.area(1.2, 1.2) == 1.44
+  end
+
+  test "geom area/2 | rectangle_width has the default value of 1" do
+    assert Etudes.Geom.area(7) == 7
+    assert Etudes.Geom.area(15) == 15
+  end
+
+  test "geom area/2 | rectangle_length has the default value of 1" do
+    assert Etudes.Geom.area() == 1
   end
 end
