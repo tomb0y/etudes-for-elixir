d2d6787796124cd2cf8c24325cf5b20d32c2263c
zs <baloghzsof@gmail.com>
2017-04-09 23:27:07 +0200

    Implement Etudes.Powers raise/3 (tail-recursive)

--- lib/etudes/powers.ex
+++ lib/etudes/powers.ex
@@ -4,8 +4,11 @@ defmodule Etudes.Powers do
   @spec raise(number(), integer()) :: number() | :undefined
   def raise(x, n) when x == 0 and n < 0, do: :undefined
   def raise(_x, n) when n == 0, do: 1
-  def raise(x, n) when n == 1, do: x
-  def raise(x, n) when n > 1, do: __MODULE__.raise(x, n - 1) * x
+  def raise(x, n) when n > 0, do: __MODULE__.raise(x, n, 1)
   def raise(x, n) when n == -1 and x != 0, do: 1 / x
   def raise(x, n) when n < -1, do: __MODULE__.raise(x, n + 1) / x
+
+  @spec raise(number(), integer(), number()) :: number()
+  def raise(_x, n, acc) when n == 0, do: acc
+  def raise(x, n, acc) when n > 0, do: __MODULE__.raise(x, n - 1, x * acc)
 end
