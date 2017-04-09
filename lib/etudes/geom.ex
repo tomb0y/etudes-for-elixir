ef712cbdaaeea1664d3f0a8b309996f0d0f274c5
zs <baloghzsof@gmail.com>
2017-04-09 17:52:28 +0200

    Change Geom area/3 into area/1
    it will now take a tuple of 3 instead of 3 separate parameters

--- lib/etudes/geom.ex
+++ lib/etudes/geom.ex
@@ -1,12 +1,16 @@
 defmodule Etudes.Geom do
   @moduledoc false
 
+  @spec area({atom(), number(), number()}) :: number()
+  def area({shape, dimension_a, dimension_b}) do
+    area(shape, dimension_a, dimension_b)
+  end
+
   @spec area(atom(), number(), number()) :: number()
-  def area(shape, dimension_a \\ 1, dimension_b \\ 1)
-  def area(shape, _, _) when not shape in [:rectangle, :triangle, :ellipse] do
+  defp area(shape, _, _) when not shape in [:rectangle, :triangle, :ellipse] do
     0
   end
-  def area(shape, dimension_a, dimension_b) when dimension_a >= 0 and dimension_b >= 0 do
+  defp area(shape, dimension_a, dimension_b) when dimension_a >= 0 and dimension_b >= 0 do
     case shape do
       :rectangle -> dimension_a * dimension_b
       :triangle -> dimension_a * dimension_b / 2
--- test/geom_test.exs
+++ test/geom_test.exs
@@ -5,76 +5,71 @@ defmodule GeomTest do
     Etudes.Geom.module_info()
   end
 
+  test "there is no exposed area/3 function in Geom" do
+    refute Enum.member?(Etudes.Geom.module_info(:exports), {:area, 3})
+  end
+
   test "area of a rectangle | yields the area of a rectangle based on it's length and width" do
-    assert Etudes.Geom.area(:rectangle, 3, 4) == 12
-    assert Etudes.Geom.area(:rectangle, 12, 7) == 84
-    assert Etudes.Geom.area(:rectangle, 1.5, 2) == 3
-    assert Etudes.Geom.area(:rectangle, 4, 1.5) == 6
-    assert Etudes.Geom.area(:rectangle, 1.2, 1.2) == 1.44
-  end
-
-  test "area of a rectangle | rectangle_width has the default value of 1" do
-    assert Etudes.Geom.area(:rectangle, 7) == 7
-    assert Etudes.Geom.area(:rectangle, 15) == 15
-  end
-
-  test "area of a rectangle | rectangle_length has the default value of 1" do
-    assert Etudes.Geom.area(:rectangle) == 1
+    assert Etudes.Geom.area({:rectangle, 3, 4}) == 12
+    assert Etudes.Geom.area({:rectangle, 12, 7}) == 84
+    assert Etudes.Geom.area({:rectangle, 1.5, 2}) == 3
+    assert Etudes.Geom.area({:rectangle, 4, 1.5}) == 6
+    assert Etudes.Geom.area({:rectangle, 1.2, 1.2}) == 1.44
   end
 
   test "area of a rectangle | only accepts non-negative rectangle_length" do
     assert_raise FunctionClauseError, fn ->
-      Etudes.Geom.area(:rectangle, -1, 1)
+      Etudes.Geom.area({:rectangle, -1, 1})
     end
   end
 
   test "area of a rectangle | only accepts non-negative rectangle_width" do
     assert_raise FunctionClauseError, fn ->
-      Etudes.Geom.area(:rectangle, 1, -1)
+      Etudes.Geom.area({:rectangle, 1, -1})
     end
   end
 
   test "area of a triangle | yields the area of a triangle based on it's base and height" do
-    assert Etudes.Geom.area(:triangle, 3, 5) == 7.5
-    assert Etudes.Geom.area(:triangle, 12, 7) == 42
-    assert Etudes.Geom.area(:triangle, 4, 1.5) == 3
-    assert Etudes.Geom.area(:triangle, 1.2, 1.2) == 0.72
+    assert Etudes.Geom.area({:triangle, 3, 5}) == 7.5
+    assert Etudes.Geom.area({:triangle, 12, 7}) == 42
+    assert Etudes.Geom.area({:triangle, 4, 1.5}) == 3
+    assert Etudes.Geom.area({:triangle, 1.2, 1.2}) == 0.72
   end
 
   test "area of a triangle | only accepts non-negative triangle_base" do
     assert_raise FunctionClauseError, fn ->
-      Etudes.Geom.area(:triangle, -1, 1)
+      Etudes.Geom.area({:triangle, -1, 1})
     end
   end
 
   test "area of a triangle | only accepts non-negative triangle_height" do
     assert_raise FunctionClauseError, fn ->
-      Etudes.Geom.area(:triangle, 1, -1)
+      Etudes.Geom.area({:triangle, 1, -1})
     end
   end
 
   test "area of an ellipse | yields the area of an ellipse based on it major and minor radiuses" do
-    assert Etudes.Geom.area(:ellipse, 2, 4) == 25.132741228718345
-    assert Etudes.Geom.area(:ellipse, 12, 7) == 263.89378290154264
-    assert Etudes.Geom.area(:ellipse, 4, 1.5) == 18.84955592153876
-    assert Etudes.Geom.area(:ellipse, 1.2, 1.2) == 4.523893421169302
+    assert Etudes.Geom.area({:ellipse, 2, 4}) == 25.132741228718345
+    assert Etudes.Geom.area({:ellipse, 12, 7}) == 263.89378290154264
+    assert Etudes.Geom.area({:ellipse, 4, 1.5}) == 18.84955592153876
+    assert Etudes.Geom.area({:ellipse, 1.2, 1.2}) == 4.523893421169302
   end
 
   test "area of an ellipse | only accepts non-negative major_radius" do
     assert_raise FunctionClauseError, fn ->
-      Etudes.Geom.area(:ellipse, -1, 1)
+      Etudes.Geom.area({:ellipse, -1, 1})
     end
   end
 
   test "area of an ellipse | only accepts non-negative minor_radius" do
     assert_raise FunctionClauseError, fn ->
-      Etudes.Geom.area(:ellipse, 1, -1)
+      Etudes.Geom.area({:ellipse, 1, -1})
     end
   end
 
   test "area of an unknown shape with whatever parameters" do
-    assert Etudes.Geom.area(:pentagon, 1, 1) == 0
-    assert Etudes.Geom.area(:circle, -1, 1) == 0
-    assert Etudes.Geom.area(:polygon, 1, -1) == 0
+    assert Etudes.Geom.area({:pentagon, 1, 1}) == 0
+    assert Etudes.Geom.area({:circle, -1, 1}) == 0
+    assert Etudes.Geom.area({:polygon, 1, -1}) == 0
   end
 end
