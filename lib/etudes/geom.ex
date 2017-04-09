b3fa01fa959da141cb724339ac2b45faf52adda7
zs <baloghzsof@gmail.com>
2017-04-09 12:40:55 +0200

    Refactor Etudes.Geom # area/2 into area/3
    it takes an atom as the first argument, that determines the type of the
    area

--- lib/etudes/geom.ex
+++ lib/etudes/geom.ex
@@ -1,9 +1,13 @@
 defmodule Etudes.Geom do
   @moduledoc false
 
-  @spec area(number(), number()) :: number()
+  @spec area(atom(), number(), number()) :: number()
+  def area(:rectangle, rectangle_length \\ 1, rectangle_width \\ 1) do
+    rectangle_area(rectangle_length, rectangle_width)
+  end
 
-  def area(rectangle_length \\ 1, rectangle_width \\ 1) do
+  @spec rectangle_area(number(), number()) :: number()
+  defp rectangle_area(rectangle_length, rectangle_width) do
     rectangle_length * rectangle_width
   end
 end
--- test/geom_test.exs
+++ test/geom_test.exs
@@ -5,26 +5,20 @@ defmodule GeomTest do
     Etudes.Geom.module_info()
   end
 
-  test "the geom module has the area/2 function" do
-    geom_methods = Etudes.Geom.module_info(:exports)
-
-    assert Enum.member?(geom_methods, { :area, 2 })
+  test "area of a rectangle | yields the area of a rectangle based on it's length and width" do
+    assert Etudes.Geom.area(:rectangle, 3, 4) == 12
+    assert Etudes.Geom.area(:rectangle, 12, 7) == 84
+    assert Etudes.Geom.area(:rectangle, 1.5, 2) == 3
+    assert Etudes.Geom.area(:rectangle, 4, 1.5) == 6
+    assert Etudes.Geom.area(:rectangle, 1.2, 1.2) == 1.44
   end
 
-  test "geom area/2 | yields the area of a rectangle" do
-    assert Etudes.Geom.area(3, 4) == 12
-    assert Etudes.Geom.area(12, 7) == 84
-    assert Etudes.Geom.area(1.5, 2) == 3
-    assert Etudes.Geom.area(4, 1.5) == 6
-    assert Etudes.Geom.area(1.2, 1.2) == 1.44
+  test "area of a rectangle | rectangle_width has the default value of 1" do
+    assert Etudes.Geom.area(:rectangle, 7) == 7
+    assert Etudes.Geom.area(:rectangle, 15) == 15
   end
 
-  test "geom area/2 | rectangle_width has the default value of 1" do
-    assert Etudes.Geom.area(7) == 7
-    assert Etudes.Geom.area(15) == 15
-  end
-
-  test "geom area/2 | rectangle_length has the default value of 1" do
-    assert Etudes.Geom.area() == 1
+  test "area of a rectangle | rectangle_length has the default value of 1" do
+    assert Etudes.Geom.area(:rectangle) == 1
   end
 end
