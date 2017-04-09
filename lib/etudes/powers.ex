08a948ba39952c3aa8cdb53d86d10fe021008004
zs <baloghzsof@gmail.com>
2017-04-09 23:38:08 +0200

    Import Kernel without raise/2 & raise/3 in Powers
    __MODULE__ didn't seem to do the trick :(

--- lib/etudes/powers.ex
+++ lib/etudes/powers.ex
@@ -1,14 +1,16 @@
 defmodule Etudes.Powers do
   @moduledoc false
 
+  import Kernel, except: [raise: 2, raise: 3]
+
   @spec raise(number(), integer()) :: number() | :undefined
   def raise(x, n) when x == 0 and n < 0, do: :undefined
   def raise(_x, n) when n == 0, do: 1
-  def raise(x, n) when n > 0, do: __MODULE__.raise(x, n, 1)
+  def raise(x, n) when n > 0, do: raise(x, n, 1)
   def raise(x, n) when n == -1 and x != 0, do: 1 / x
-  def raise(x, n) when n < -1, do: __MODULE__.raise(x, n + 1) / x
+  def raise(x, n) when n < -1, do: raise(x, n + 1) / x
 
   @spec raise(number(), integer(), number()) :: number()
-  def raise(_x, n, acc) when n == 0, do: acc
-  def raise(x, n, acc) when n > 0, do: __MODULE__.raise(x, n - 1, x * acc)
+  defp raise(_x, n, acc) when n == 0, do: acc
+  defp raise(x, n, acc) when n > 0, do: raise(x, n - 1, x * acc)
 end
